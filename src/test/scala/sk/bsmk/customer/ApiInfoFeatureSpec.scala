4c86c26d1c20161017f5abe630881ef31b3dbe22
Miroslav Matejovsky <miroslav.matejovsky@wirecard.com>
2017-11-01 11:37:52 +0100

    added simple test

---
+++ src/test/scala/sk/bsmk/customer/ApiInfoFeatureSpec.scala
@@ -0,0 +1,43 @@
+package sk.bsmk.customer
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Route
+import sk.bsmk.customer.api.CustomerApi
+
+class ApiInfoFeatureSpec extends ApiFeatureSpec {
+
+  private val smallRoute = CustomerApi.routes
+
+  "The service" should {
+
+    "return a greeting for GET requests to the root path" in {
+      // tests:
+      Get() ~> smallRoute ~> check {
+        responseAs[String] shouldEqual "Captain on the bridge!"
+      }
+    }
+
+    "return a 'PONG!' response for GET requests to /ping" in {
+      // tests:
+      Get("/ping") ~> smallRoute ~> check {
+        responseAs[String] shouldEqual "PONG!"
+      }
+    }
+
+    "leave GET requests to other paths unhandled" in {
+      // tests:
+      Get("/kermit") ~> smallRoute ~> check {
+        handled shouldBe false
+      }
+    }
+
+    "return a MethodNotAllowed error for PUT requests to the root path" in {
+      // tests:
+      Put() ~> Route.seal(smallRoute) ~> check {
+        status shouldEqual StatusCodes.MethodNotAllowed
+        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
+      }
+    }
+  }
+
+}
