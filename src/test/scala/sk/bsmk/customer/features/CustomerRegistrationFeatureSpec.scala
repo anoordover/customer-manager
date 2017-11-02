1c3e3a58b6ad1f09f6e9e7ef257aab72c8fb9774
Miroslav Matejovsky <miroslav.matejovsky@wirecard.com>
2017-11-02 07:26:21 +0100

    added test for registration

--- src/test/scala/sk/bsmk/customer/features/CustomerRegistrationFeatureSpec.scala
+++ src/test/scala/sk/bsmk/customer/features/CustomerRegistrationFeatureSpec.scala
@@ -1,18 +1,28 @@
 package sk.bsmk.customer.features
 
-import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model._
 import sk.bsmk.customer.ApiFeatureSpec
-import sk.bsmk.customer.api.{CustomerListResponse, CustomerRegistrationRequest, JsonSupport}
+import sk.bsmk.customer.api.CustomerRegistrationRequest
 
 class CustomerRegistrationFeatureSpec extends ApiFeatureSpec {
 
-//  "The customers resource" should {
-//    "register customer for POST request" in {
-//      val request = CustomerRegistrationRequest("some@mail.com")
-//      Post("/api/customers", request) ~> route ~> check {
-//        status shouldEqual StatusCodes.OK
-//      }
-//    }
-//  }
+  "The customer registration endpoint" when {
+    "accessed with POST request and valid body" should {
+
+      val futureResp = Marshal(CustomerRegistrationRequest("some@email.com")).to[RequestEntity].flatMap { entity ⇒
+        val request = HttpRequest(
+          method = HttpMethods.POST,
+          uri = s"$BaseUri/api/customers",
+          entity = entity
+        )
+        Http().singleRequest(request)
+      }
+
+      shouldReturnOk(futureResp)
+
+    }
+  }
 
 }
